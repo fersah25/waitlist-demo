d3a35e90f87fb950659e6c423d16910c9acc5458
fersah25 <152728327+fersah25@users.noreply.github.com>
2026-02-19 02:23:14 +0300

    upp

--- app/rootProvider.tsx
+++ app/rootProvider.tsx
@@ -2,17 +2,16 @@
 
 import { ReactNode, useEffect } from "react";
 import sdk from "@farcaster/frame-sdk";
-import { base } from "viem/chains";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { base } from "viem/chains";
 
 export function RootProvider({ children }: { children: ReactNode }) {
-
   useEffect(() => {
     const init = async () => {
       try {
         await sdk.actions.ready();
-      } catch (error) {
-        console.error("Farcaster SDK error:", error);
+      } catch (e) {
+        console.error(e);
       }
     };
     init();
@@ -23,8 +22,7 @@ export function RootProvider({ children }: { children: ReactNode }) {
       apiKey={process.env.NEXT_PUBLIC_ONCHAIN_KIT_API_KEY}
       chain={base}
       config={{
-        // MiniKit hatasını bu blok çözecek
-        paymaster: "",
+        appearance: { mode: "auto" }
       }}
     >
       {children}
